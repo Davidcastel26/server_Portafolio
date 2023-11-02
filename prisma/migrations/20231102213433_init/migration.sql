181437510b3a2a7df04f2f0d0fb9f41071c4abb1
David Castellanos <davcastellanoslarios@gmail.com>
2023-11-02 15:37:47 -0600

    creating get user route

---
+++ prisma/migrations/20231102213433_init/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Project` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "userId" VARCHAR(45) NOT NULL;
+
+-- CreateIndex
+CREATE INDEX "Project_userId_idx" ON "Project"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("idUser") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/controllers/mainControllers/users.controllers.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Response, Request } from "express";
+import prismaDb from "../../models/prismaDb";
+
+
+export const getAllUsers = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+
+    try {
+
+        const allUsers = await prismaDb.user.findMany({
+            include:{
+                tags: true,
+                about: true,
+                projects: true
+            }
+        })
+        
+        return res.status(200).json(allUsers)
+
+    } catch (error) {
+        next(error)
+    }
+    
+}
